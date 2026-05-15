4823fcea53660d8acb6082f80d87ed21dae583c9
PedroFdzCode <pedrofdezs01@gmail.com>
2026-05-15 20:33:34 +0200

    Fix: text prefix added

--- tests/test_modelo.py
+++ tests/test_modelo.py
@@ -4,14 +4,14 @@ import os
 
 BASE = os.path.dirname(os.path.abspath(__file__))
 
-def load_model():
+def test_load_model():
     model = joblib.load(BASE, "models/model.pkl")
     vectorizer = joblib.load(BASE, "models/vectorizer.pkl")
     assert model is not None
     assert vectorizer is not None
 
 
-def predict_labels():
+def test_predict_labels():
     model = joblib.load("models/model.pkl")
     vectorizer = joblib.load("models/vectorizer.pkl")
     vector = vectorizer.transform(["great product"])
@@ -19,14 +19,14 @@ def predict_labels():
 
 
 #Accuracy
-def accuracy_positive_low():
+def test_accuracy_positive_low():
     model = joblib.load(os.path.join(BASE, "..", "models", "model.pkl"))
     vectorizer = joblib.load(os.path.join(BASE, "..", "models", "vectorizer.pkl"))
     vector = vectorizer.transform(["Yeah this is good!"])
     print(model.predict_proba(vector))
     assert model.predict_proba(vector)[0][1] < 0.70
 
-def accuracy_positive_high():
+def test_accuracy_positive_high():
     model = joblib.load(os.path.join(BASE, "..", "models", "model.pkl"))
     vectorizer = joblib.load(os.path.join(BASE, "..", "models","vectorizer.pkl"))
     vector = vectorizer.transform(["Exactly what i wanted"])
@@ -34,14 +34,14 @@ def accuracy_positive_high():
     assert model.predict_proba(vector)[0][1] > 0.70
 
 
-def accuracy_negative_low():
+def test_accuracy_negative_low():
     model = joblib.load(os.path.join(BASE, "..", "models", "model.pkl"))
     vectorizer = joblib.load(os.path.join(BASE, "..", "models", "vectorizer.pkl"))
     vector = vectorizer.transform(["it is a bit defective, but works at least"])
     print(model.predict_proba(vector)[0][0])
     assert model.predict_proba(vector)[0][0] < 0.70
 
-def accuracy_negative_high():
+def test_accuracy_negative_high():
     model = joblib.load(os.path.join(BASE, "..", "models", "model.pkl"))
     vectorizer = joblib.load(os.path.join(BASE, "..", "models", "vectorizer.pkl"))
     vector = vectorizer.transform(["Totally useless"])
